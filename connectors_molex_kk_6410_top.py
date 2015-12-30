8491c24b637d48242a79dce549308aa43d8688c9
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2015-12-30 11:47:18 +0100

    [enh] improve molex kk_6410 connector

--- connectors_molex_kk_6410_top.py
+++ connectors_molex_kk_6410_top.py
@@ -12,78 +12,79 @@ args = parser.parse_args()
 
 pincount = int(args.pincount[0])
 pad_spacing = 2.54
-start_pos_x = -(pincount-1)*pad_spacing
-end_pos_x = 0
+start_pos_x = 0
+end_pos_x = (pincount-1)*pad_spacing
 
 # Through-hole type shrouded header, Top entry type
 footprint_name = 'Connectors_Molex_KK_6410-{pincount:02g}'.format(pincount=pincount) # TODO: name
 
 kicad_mod = KicadMod(footprint_name)
 kicad_mod.setDescription("Connector Headers with Friction Lock, 22-27-2{pincount:02g}1".format(pincount=pincount))
+kicad_mod.setTags('connector molex kk_6410')
 
 # set general values
-kicad_mod.addText('reference', 'CON**', {'x':start_pos_x+1, 'y':-4}, 'F.SilkS')
-kicad_mod.addText('value', footprint_name, {'x':-(pincount-1)*pad_spacing/2., 'y':4}, 'F.Fab')
+kicad_mod.addText('reference', 'CON**', {'x':start_pos_x+1, 'y':4}, 'F.SilkS')
+kicad_mod.addText('value', footprint_name, {'x':(pincount-1)*pad_spacing/2., 'y':-4}, 'F.Fab')
 
 # create Silkscreen
 
-kicad_mod.addRectLine({'x':start_pos_x-2.54/2, 'y':2.92}, {'x':end_pos_x+2.54/2, 'y':-2.88}, 'F.SilkS', 0.15)
+kicad_mod.addRectLine({'x':start_pos_x-2.54/2-0.1, 'y':-3.02}, {'x':end_pos_x+2.54/2+0.1, 'y':2.98}, 'F.SilkS', 0.15)
 
 #ramps
 if pincount <= 6:
     # single ramp
-    kicad_mod.addPolygoneLine([{'x':start_pos_x, 'y':-2.88}
-                              ,{'x':start_pos_x, 'y':-1.88}
-                              ,{'x':end_pos_x, 'y':-1.88}
-                              ,{'x':end_pos_x, 'y':-2.88}], 'F.SilkS', 0.15)
-    kicad_mod.addPolygoneLine([{'x':start_pos_x, 'y':-1.88}
-                              ,{'x':start_pos_x+0.25, 'y':-1.45}
-                              ,{'x':end_pos_x-0.25, 'y':-1.45}
-                              ,{'x':end_pos_x, 'y':-1.88}], 'F.SilkS', 0.15)
-    kicad_mod.addLine({'x':start_pos_x+0.25, 'y':-2.88}, {'x':start_pos_x+0.25, 'y':-1.88}, 'F.SilkS', 0.15)
-    kicad_mod.addLine({'x':end_pos_x-0.25, 'y':-2.88}, {'x':end_pos_x-0.25, 'y':-1.88}, 'F.SilkS', 0.15)
+    kicad_mod.addPolygoneLine([{'x':start_pos_x, 'y':2.98}
+                              ,{'x':start_pos_x, 'y':1.98}
+                              ,{'x':end_pos_x, 'y':1.98}
+                              ,{'x':end_pos_x, 'y':2.98}], 'F.SilkS', 0.15)
+    kicad_mod.addPolygoneLine([{'x':start_pos_x, 'y':1.98}
+                              ,{'x':start_pos_x+0.25, 'y':1.55}
+                              ,{'x':end_pos_x-0.25, 'y':1.55}
+                              ,{'x':end_pos_x, 'y':1.98}], 'F.SilkS', 0.15)
+    kicad_mod.addLine({'x':start_pos_x+0.25, 'y':2.98}, {'x':start_pos_x+0.25, 'y':1.98}, 'F.SilkS', 0.15)
+    kicad_mod.addLine({'x':end_pos_x-0.25, 'y':2.98}, {'x':end_pos_x-0.25, 'y':1.98}, 'F.SilkS', 0.15)
 else:
     # two ramps
-    kicad_mod.addPolygoneLine([{'x':start_pos_x, 'y':-2.88}
-                              ,{'x':start_pos_x, 'y':-1.88}
-                              ,{'x':start_pos_x+2*pad_spacing, 'y':-1.88}
-                              ,{'x':start_pos_x+2*pad_spacing, 'y':-2.88}], 'F.SilkS', 0.15)
-    kicad_mod.addPolygoneLine([{'x':start_pos_x, 'y':-1.88}
-                              ,{'x':start_pos_x+0.25, 'y':-1.45}
-                              ,{'x':start_pos_x+2*pad_spacing-0.25, 'y':-1.45}
-                              ,{'x':start_pos_x+2*pad_spacing, 'y':-1.88}], 'F.SilkS', 0.15)
-    kicad_mod.addLine({'x':start_pos_x+0.25, 'y':-2.88}, {'x':start_pos_x+0.25, 'y':-1.88}, 'F.SilkS', 0.15)
-    kicad_mod.addLine({'x':start_pos_x+2*pad_spacing-0.25, 'y':-2.88}, {'x':start_pos_x+2*pad_spacing-0.25, 'y':-1.88}, 'F.SilkS', 0.15)
+    kicad_mod.addPolygoneLine([{'x':start_pos_x, 'y':2.98}
+                              ,{'x':start_pos_x, 'y':1.98}
+                              ,{'x':start_pos_x+2*pad_spacing, 'y':1.98}
+                              ,{'x':start_pos_x+2*pad_spacing, 'y':2.98}], 'F.SilkS', 0.15)
+    kicad_mod.addPolygoneLine([{'x':start_pos_x, 'y':1.98}
+                              ,{'x':start_pos_x+0.25, 'y':1.55}
+                              ,{'x':start_pos_x+2*pad_spacing, 'y':1.55}
+                              ,{'x':start_pos_x+2*pad_spacing, 'y':1.98}], 'F.SilkS', 0.15)
+    kicad_mod.addLine({'x':start_pos_x+0.25, 'y':2.98}, {'x':start_pos_x+0.25, 'y':1.98}, 'F.SilkS', 0.15)
+    #kicad_mod.addLine({'x':start_pos_x+2*pad_spacing-0.25, 'y':2.98}, {'x':start_pos_x+2*pad_spacing-0.25, 'y':1.98}, 'F.SilkS', 0.15)
     
-    kicad_mod.addPolygoneLine([{'x':end_pos_x, 'y':-2.88}
-                              ,{'x':end_pos_x, 'y':-1.88}
-                              ,{'x':end_pos_x-2*pad_spacing, 'y':-1.88}
-                              ,{'x':end_pos_x-2*pad_spacing, 'y':-2.88}], 'F.SilkS', 0.15)
-    kicad_mod.addPolygoneLine([{'x':end_pos_x, 'y':-1.88}
-                              ,{'x':end_pos_x-0.25, 'y':-1.45}
-                              ,{'x':end_pos_x-2*pad_spacing+0.25, 'y':-1.45}
-                              ,{'x':end_pos_x-2*pad_spacing, 'y':-1.88}], 'F.SilkS', 0.15)
-    kicad_mod.addLine({'x':end_pos_x-2*pad_spacing+0.25, 'y':-2.88}, {'x':end_pos_x-2*pad_spacing+0.25, 'y':-1.88}, 'F.SilkS', 0.15)
-    kicad_mod.addLine({'x':end_pos_x-0.25, 'y':-2.88}, {'x':end_pos_x-0.25, 'y':-1.88}, 'F.SilkS', 0.15)
+    kicad_mod.addPolygoneLine([{'x':end_pos_x, 'y':2.98}
+                              ,{'x':end_pos_x, 'y':1.98}
+                              ,{'x':end_pos_x-2*pad_spacing, 'y':1.98}
+                              ,{'x':end_pos_x-2*pad_spacing, 'y':2.98}], 'F.SilkS', 0.15)
+    kicad_mod.addPolygoneLine([{'x':end_pos_x, 'y':1.98}
+                              ,{'x':end_pos_x-0.25, 'y':1.55}
+                              ,{'x':end_pos_x-2*pad_spacing, 'y':1.55}
+                              ,{'x':end_pos_x-2*pad_spacing, 'y':1.98}], 'F.SilkS', 0.15)
+    #kicad_mod.addLine({'x':end_pos_x-2*pad_spacing+0.25, 'y':2.98}, {'x':end_pos_x-2*pad_spacing+0.25, 'y':1.98}, 'F.SilkS', 0.15)
+    kicad_mod.addLine({'x':end_pos_x-0.25, 'y':2.98}, {'x':end_pos_x-0.25, 'y':1.98}, 'F.SilkS', 0.15)
 
 for i in range(0, pincount):
     middle_x = start_pos_x+i*pad_spacing
     start_x = middle_x-1.6/2
     end_x = middle_x+1.6/2
-    kicad_mod.addPolygoneLine([{'x':start_x, 'y':2.92}
-                              ,{'x':start_x, 'y':2.3}
-                              ,{'x':end_x, 'y':2.3}
-                              ,{'x':end_x, 'y':2.92}], 'F.SilkS', 0.15)
+    kicad_mod.addPolygoneLine([{'x':start_x, 'y':-3.02}
+                              ,{'x':start_x, 'y':-2.4}
+                              ,{'x':end_x, 'y':-2.4}
+                              ,{'x':end_x, 'y':-3.02}], 'F.SilkS', 0.15)
 
 def round_to(n, precision):
     correction = 0.5 if n >= 0 else -0.5
     return int( n/precision+correction ) * precision
 
 # create Courtyard
-kicad_mod.addRectLine({'x':round_to(start_pos_x-2.54/2-0.25-0.03, 0.05), 'y':2.92+0.25+0.03}, {'x':round_to(end_pos_x+2.54/2+0.25+0.03, 0.05), 'y':-2.88-0.25-0.02}, 'F.CrtYd', 0.05)
+kicad_mod.addRectLine({'x':round_to(start_pos_x-2.54/2-0.5-0.03-0.1, 0.05), 'y':2.98+0.5+0.02}, {'x':round_to(end_pos_x+2.54/2+0.5+0.03+0.1, 0.05), 'y':-3.02-0.5-0.03}, 'F.CrtYd', 0.05)
 
 # create pads
-createNumberedPadsTHT(kicad_mod, pincount, -1*pad_spacing, 1, {'x':1.6, 'y':2.3}) # TODO pad?
+createNumberedPadsTHT(kicad_mod, pincount, pad_spacing, 1.2, {'x':2, 'y':2.6}) # TODO pad?
 
 # save model
 kicad_mod.save('Connectors_Molex.pretty/{footprint_name}.kicad_mod'.format(footprint_name=footprint_name))
