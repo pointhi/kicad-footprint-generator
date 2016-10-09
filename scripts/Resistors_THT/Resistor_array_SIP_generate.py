45fc8181e65dd23b3c61df23ee3b6d9a20b5a365
jkriege2 <jan@jkrieger.de>
2016-10-09 10:45:54 +0200

    now scripts find main lib

--- scripts/Resistors_THT/Resistor_array_SIP_generate.py
+++ scripts/Resistors_THT/Resistor_array_SIP_generate.py
@@ -8,6 +8,7 @@ import math
 #sys.path.append(os.environ.get('KIFOOTPRINTGENERATOR'))  # enable package import from parent directory
 #sys.path.append("D:\hardware\KiCAD\kicad-footprint-generator")  # enable package import from parent directory
 sys.path.append(os.path.join(sys.path[0],"..","..","kicad_mod")) # load kicad_mod path
+sys.path.append(os.path.join(sys.path[0],"..","..")) # load kicad_mod path
 
 from KicadModTree import *  # NOQA
 
