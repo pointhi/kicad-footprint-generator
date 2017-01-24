9c4b168b966fa4602fb4194b1710e1a4e326d550
jkriege2 <jan@jkrieger.de>
2017-01-24 22:35:19 +0100

    added SMD-pinheaders scripts

--- scripts/tools/footprint_scripts_pin_headers.py
+++ scripts/tools/footprint_scripts_pin_headers.py
@@ -31,7 +31,7 @@ def makePinHeadStraight(rows, cols, rm, coldist, package_width, overlen_top, ove
     h_crt = max(h_fab, (rows - 1) * rm + pad[1]) + 2 * crt_offset
     l_crt = coldist * (cols - 1) / 2 - w_crt / 2
     t_crt = (rows - 1) * rm / 2 - h_crt / 2
-
+    
     # if rm == 2.54:
     #    footprint_name = "Pin_Header_Straight_{0}x{1:02}".format(cols, rows)
     # else:
@@ -301,6 +301,151 @@ def makePinHeadAngled(rows, cols, rm, coldist, pack_width, pack_offset, pin_leng
     # add model
     kicad_modg.append(
         Model(filename=lib_name + ".3dshapes/" + footprint_name + ".wrl", at=offset3d, scale=scale3d, rotate=rotate3d))
+    
+    # print render tree
+    # print(kicad_mod.getRenderTree())
+    # print(kicad_mod.getCompleteRenderTree())
+    
+    # write file
+    file_handler = KicadFileHandler(kicad_mod)
+    file_handler.writeFile(footprint_name + '.kicad_mod')
+
+
+def makePinHeadStraightSMD(rows, cols, rm, coldist, rmx_pad_offset,rmx_pin_length, pin_width, package_width, overlen_top, overlen_bottom, pad,
+                        start_left=True, tags_additional=[], lib_name="Pin_Headers", offset3d=[0, 0, 0], scale3d=[1, 1, 1],
+                        rotate3d=[0, 0, 0]):
+    ddrill=0.5
+    h_fab = (rows - 1) * rm + overlen_top + overlen_bottom
+    w_fab = package_width
+    l_fab = (coldist * (cols - 1) - w_fab) / 2
+    t_fab = -overlen_top
+    
+    h_slk = h_fab + 2 * slk_offset
+    w_slk = max(w_fab + 2 * slk_offset, coldist * (cols - 1) - pad[0] - 4 * slk_offset)
+    l_slk = (coldist * (cols - 1) - w_slk) / 2
+    t_slk = -overlen_top - slk_offset
+    
+    w_crt = max(package_width, coldist * (cols - 1)+2*rmx_pad_offset + pad[0]) + 2 * crt_offset
+    h_crt = max(h_fab, (rows - 1) * rm + pad[1]) + 2 * crt_offset
+    l_crt = coldist * (cols - 1) / 2 - w_crt / 2
+    t_crt = (rows - 1) * rm / 2 - h_crt / 2
+    
+    # if rm == 2.54:
+    #    footprint_name = "Pin_Header_Straight_{0}x{1:02}".format(cols, rows)
+    # else:
+    footprint_name = "Pin_Header_Straight_{0}x{1:02}_Pitch{2:03.2f}mm_SMD".format(cols, rows, rm)
+    
+    description = "Through hole straight pin header, {0}x{1:02}, {2:03.2f}mm pitch".format(cols, rows, rm)
+    tags = "Through hole pin header THT {0}x{1:02} {2:03.2f}mm".format(cols, rows, rm)
+    if (cols == 1):
+        description = description + ", single row"
+        tags = tags + " single row"
+        if start_left:
+            description = description + ", style 1 (pin 1 left)"
+            tags = tags + " style1 pin1 left"
+            footprint_name = footprint_name + "_style1"
+        else:
+            description = description + ", style 2 (pin 1 right)"
+            tags = tags + " style2 pin1 right"
+            footprint_name = footprint_name + "_style2"
+    elif (cols == 2):
+        description = description + ", double rows"
+        tags = tags + " double row"
+
+
+
+    if (len(tags_additional) > 0):
+        for t in tags_additional:
+            footprint_name = footprint_name + "_" + t
+            description = description + ", " + t
+            tags = tags + " " + t
+    
+    print(footprint_name)
+    
+    # init kicad footprint
+    kicad_mod = Footprint(footprint_name)
+    kicad_mod.setDescription(description)
+    kicad_mod.setTags(tags)
+    kicad_mod.setAttribute('smd')
+    
+    # anchor for SMD-symbols is in the center, for THT-sybols at pin1
+    offset = [-(cols-1)*coldist/2, -(rows-1)*rm/2.0]
+    
+    kicad_modg = Translation(offset[0], offset[1])
+    kicad_mod.append(kicad_modg)
+    
+    # set general values
+    kicad_modg.append(
+        Text(type='reference', text='REF**', at=[coldist * (cols - 1) / 2, t_slk - txt_offset], layer='F.SilkS'))
+    kicad_modg.append(Text(type='value', text=footprint_name, at=[coldist * (cols - 1) / 2, t_slk + h_slk + txt_offset],
+                           layer='F.Fab'))
+
+    cleft = range(0, rows, 2)
+    cright = range(1, rows, 2)
+    if not start_left:
+        cleft = range(1, rows, 2)
+        cright = range(0, rows, 2)
+
+    # create FAB-layer
+    kicad_modg.append(RectLine(start=[l_fab, t_fab], end=[l_fab + w_fab, t_fab + h_fab], layer='F.Fab', width=lw_fab))
+    if cols==1:
+        for c in cleft:
+            kicad_modg.append(RectLine(start=[l_fab, c*rm-pin_width/2], end=[ -rmx_pin_length, c*rm+pin_width/2], layer='F.Fab', width=lw_fab))
+        for c in cright:
+            kicad_modg.append(RectLine(start=[l_fab+w_fab, c*rm-pin_width/2], end=[rmx_pin_length, c*rm+pin_width/2], layer='F.Fab', width=lw_fab))
+    elif cols == 2:
+        for c in range(0,rows):
+            kicad_modg.append(RectLine(start=[l_fab, c * rm - pin_width / 2], end=[ - rmx_pin_length, c * rm + pin_width/2],layer='F.Fab', width=lw_fab))
+            kicad_modg.append(RectLine(start=[l_fab + w_fab, c * rm - pin_width / 2], end=[rm + rmx_pin_length, c * rm + pin_width/2], layer='F.Fab', width=lw_fab))
+
+    # create SILKSCREEN-layer + pin1 marker
+    kicad_modg.append(PolygoneLine(polygone=[[l_slk, -(pad[1] / 2 + 2*lw_slk + slk_offset)], [l_slk, t_slk], [l_slk + w_slk, t_slk], [l_slk + w_slk, -(pad[1] / 2 + 2*lw_slk + slk_offset)]], layer='F.SilkS', width=lw_slk))
+    kicad_modg.append(PolygoneLine(polygone=[[l_slk, (rows-1)*rm + (pad[1] / 2 + 2*lw_slk + slk_offset)], [l_slk, t_slk + h_slk], [l_slk + w_slk, t_slk + h_slk], [l_slk + w_slk, (rows-1)*rm + (pad[1] / 2 + 2*lw_slk + slk_offset)]], layer='F.SilkS', width=lw_slk))
+    if (cols == 1):
+        silkh=2*rm-pad[1]-4*lw_slk+2*slk_offset
+        for c in cleft:
+            kicad_modg.append(Line(start=[l_slk+w_slk, max(t_slk, c * rm - silkh/2)], end=[l_slk+w_slk, min(t_slk+h_slk,c * rm + silkh / 2)], layer='F.SilkS', width=lw_slk))
+            if (c+1)==1:
+                kicad_modg.append(Line(start=[l_slk , c*rm-pad[1]/2- 2*lw_slk - slk_offset], end=[-rmx_pad_offset-pad[0]/2 , c*rm-pad[1]/2- 2*lw_slk - slk_offset], layer='F.SilkS', width=lw_slk))
+        for c in cright:
+            kicad_modg.append(Line(start=[l_slk , max(t_slk, c * rm - silkh / 2)],end=[l_slk, min(t_slk+h_slk,c * rm + silkh / 2)], layer='F.SilkS',width=lw_slk))
+            if (c + 1) == 1:
+                kicad_modg.append(Line(start=[l_slk+w_slk, c * rm - pad[1] / 2 - 2 * lw_slk - slk_offset],
+                                       end=[rmx_pad_offset + pad[0] / 2, c * rm - pad[1] / 2 - 2 * lw_slk - slk_offset],
+                                       layer='F.SilkS', width=lw_slk))
+    if (cols==2):
+        kicad_modg.append(Line(start=[-pad[0]/2-rmx_pad_offset, -(pad[1] / 2 + 2*lw_slk + slk_offset)], end=[l_slk, -(pad[1] / 2 + 2*lw_slk + slk_offset)], layer='F.SilkS', width=lw_slk))
+    
+    # create courtyard
+    kicad_mod.append(RectLine(start=[roundCrt(l_crt + offset[0]), roundCrt(t_crt + offset[1])],
+                              end=[roundCrt(l_crt + offset[0] + w_crt), roundCrt(t_crt + offset[1] + h_crt)],
+                              layer='F.CrtYd', width=lw_crt))
+    
+    # create pads
+    p1 = int(1)
+    x1 = 0
+    y1 = 0
+    
+    pad_type = Pad.TYPE_SMT
+    pad_shape1 = Pad.SHAPE_RECT
+    pad_layers = 'F'
+    
+    if cols==1:
+        for c in cleft:
+            kicad_modg.append(Pad(number=c+1, type=pad_type, shape=pad_shape1, at=[-rmx_pad_offset, c*rm], size=pad, drill=ddrill,layers=[pad_layers + '.Cu', pad_layers + '.Mask']))
+        for c in cright:
+            kicad_modg.append(Pad(number=c+1, type=pad_type, shape=pad_shape1, at=[rmx_pad_offset, c * rm], size=pad, drill=ddrill,layers=[pad_layers + '.Cu', pad_layers + '.Mask']))
+    elif cols==2:
+        p = 1
+        for c in range(0,rows):
+            kicad_modg.append(Pad(number=p, type=pad_type, shape=pad_shape1, at=[-rmx_pad_offset, c * rm], size=pad, drill=ddrill, layers=[pad_layers + '.Cu', pad_layers + '.Mask']))
+            p=p+1
+            kicad_modg.append(Pad(number=p, type=pad_type, shape=pad_shape1, at=[coldist+rmx_pad_offset, c * rm], size=pad, drill=ddrill,layers=[pad_layers + '.Cu', pad_layers + '.Mask']))
+            p=p+1
+
+    
+    # add model
+    kicad_modg.append(Model(filename=lib_name + ".3dshapes/" + footprint_name + ".wrl", at=offset3d, scale=scale3d, rotate=rotate3d))
     
     # print render tree
     # print(kicad_mod.getRenderTree())
