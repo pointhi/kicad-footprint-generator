7124ff409ca554d3be537fd0577c76c524f8d568
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2017-02-19 21:06:39 +0100

    Update copyright header, to be excluded in sphinx documentation

--- KicadModTree/KicadFileHandler.py
+++ KicadModTree/KicadFileHandler.py
@@ -1,19 +1,17 @@
-'''
-kicad-footprint-generator is free software: you can redistribute it and/or
-modify it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-kicad-footprint-generator is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with kicad-footprint-generator. If not, see < http://www.gnu.org/licenses/ >.
-
-(C) 2016 by Thomas Pointhuber, <thomas.pointhuber@gmx.at>
-'''
+# KicadModTree is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# KicadModTree is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with kicad-footprint-generator. If not, see < http://www.gnu.org/licenses/ >.
+#
+# (C) 2016 by Thomas Pointhuber, <thomas.pointhuber@gmx.at>
 
 from KicadModTree.FileHandler import FileHandler
 from KicadModTree.util.kicad_util import *
@@ -38,6 +36,9 @@ def get_layer_width(layer, width=None):
 
 
 class KicadFileHandler(FileHandler):
+    '''
+    Write our generated Tree into a .kicad_mod file
+    '''
     def __init__(self, kicad_mod):
         FileHandler.__init__(self, kicad_mod)
 
