8c5e22e17f605dc49093c7ad8955b495510523f4
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2017-03-23 23:42:29 +0100

    Fix PEP8

--- KicadModTree/examples/padArrayWithOutline.py
+++ KicadModTree/examples/padArrayWithOutline.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-#!/usr/bin/env python
-
 # KicadModTree is free software: you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
