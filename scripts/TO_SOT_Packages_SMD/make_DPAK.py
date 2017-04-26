3c472998722a0e0e89f8c39de67bd12a2698faf3
hackscribble <hackscribble@outlook.com>
2017-04-26 14:14:21 +0100

    Finished refactoring for now.

--- scripts/TO_SOT_Packages_SMD/make_DPAK.py
+++ scripts/TO_SOT_Packages_SMD/make_DPAK.py
@@ -87,9 +87,26 @@ def build_footprint(base, variant, cut_pin=False, tab_linked=False):
     SPLIT_PASTE = (base['footprint']['split_paste'] == 'on')
 
 
+    def add_properties(m):
+        m.setDescription('{bd:s}, {vd:s}'.format(bd=base['description'], vd=variant['datasheet']))
+        m.setTags('{bk:s} {vk:s}'.format(bk=base['keywords'], vk=variant['keywords']))
+        m.setAttribute('smd')
+        return m
+
+
+    def add_labels(m):
+        m.append(Text(type='reference', text='REF**', size=[1,1], at=[LABEL_X_MM, -LABEL_Y_MM], layer='F.SilkS'))
+        m.append(Text(type='user', text='%R', size=[1,1], at=[0, 0], layer='F.Fab'))
+        m.append(Text(type='value', text=NAME, at=[LABEL_X_MM, LABEL_Y_MM], layer='F.Fab'))
+        return m
+
+
     def draw_tab(m, layer):
-        tab_outline = [[DEVICE_OFFSET_X_MM - TAB_X_MM, -TAB_OFFSET_Y_MM], [DEVICE_OFFSET_X_MM, -TAB_OFFSET_Y_MM],\
-                       [DEVICE_OFFSET_X_MM, TAB_OFFSET_Y_MM], [DEVICE_OFFSET_X_MM - TAB_X_MM, TAB_OFFSET_Y_MM]]
+        right_x = DEVICE_OFFSET_X_MM
+        left_x = right_x - TAB_X_MM
+        top_y = -TAB_OFFSET_Y_MM
+        bottom_y = -top_y
+        tab_outline = [[left_x, top_y], [right_x, top_y], [right_x, bottom_y], [left_x, bottom_y]]
         m.append(PolygoneLine(polygone=tab_outline, layer=layer, width=FAB_LINE_WIDTH_MM))
         return m
 
@@ -115,8 +132,8 @@ def build_footprint(base, variant, cut_pin=False, tab_linked=False):
             if not (pin == CENTRE_PIN and cut_pin):
                 top_y = PAD_1_Y_MM + ((pin - 1) * variant['pitch_mm']) - (variant['pin']['y_mm'] / 2.0)
                 bottom_y = PAD_1_Y_MM + ((pin - 1) * variant['pitch_mm']) + (variant['pin']['y_mm'] / 2.0)
-                pin_outline = [[right_x + (pin_1_extend_mm if pin == 1 else 0), top_y], [left_x , top_y],\
-                               [left_x, bottom_y], [right_x, bottom_y]]
+                pin_outline = [[right_x + (pin_1_extend_mm if pin == 1 else 0), top_y],\
+                               [left_x , top_y], [left_x, bottom_y], [right_x, bottom_y]]
                 kicad_mod.append(PolygoneLine(polygone=pin_outline, layer=layer, width=FAB_LINE_WIDTH_MM))
         return m
 
@@ -129,77 +146,68 @@ def build_footprint(base, variant, cut_pin=False, tab_linked=False):
 
 
     def draw_markers(m, layer):
-        top_marker = [[DEVICE_OFFSET_X_MM - TAB_X_MM - BODY_X_MM + 1.3, -BODY_OFFSET_Y_MM - SILK_LINE_NUDGE],\
-                       [DEVICE_OFFSET_X_MM - TAB_X_MM - BODY_X_MM - SILK_LINE_NUDGE, -BODY_OFFSET_Y_MM - SILK_LINE_NUDGE],\
-                       [DEVICE_OFFSET_X_MM - TAB_X_MM - BODY_X_MM - SILK_LINE_NUDGE, PAD_1_Y_MM - variant['pad']['y_mm'] / 2.0 - 1.5 * SILK_LINE_NUDGE],
-                       [PAD_1_X_MM - variant['pad']['x_mm'] / 2.0, PAD_1_Y_MM - variant['pad']['y_mm'] / 2.0 - 1.5 * SILK_LINE_NUDGE]]
-        bottom_marker = [[DEVICE_OFFSET_X_MM - TAB_X_MM - BODY_X_MM + 1.3, BODY_OFFSET_Y_MM + SILK_LINE_NUDGE],\
-                         [DEVICE_OFFSET_X_MM - TAB_X_MM - BODY_X_MM - SILK_LINE_NUDGE, BODY_OFFSET_Y_MM + SILK_LINE_NUDGE],\
-                         [DEVICE_OFFSET_X_MM - TAB_X_MM - BODY_X_MM - SILK_LINE_NUDGE, -PAD_1_Y_MM + variant['pad']['y_mm'] / 2.0 + 1.5 * SILK_LINE_NUDGE],\
-                         [DEVICE_OFFSET_X_MM - TAB_X_MM - BODY_X_MM - 1.3, -PAD_1_Y_MM + variant['pad']['y_mm'] / 2.0 + 1.5 * SILK_LINE_NUDGE]]
+        magic_number = 1.3  # TODO needs better name
+        other_magic_number = 1.5  #  TODO needs better name
+        right_x = DEVICE_OFFSET_X_MM - TAB_X_MM - BODY_X_MM + magic_number
+        middle_x = DEVICE_OFFSET_X_MM - TAB_X_MM - BODY_X_MM - SILK_LINE_NUDGE
+        left_x = PAD_1_X_MM - variant['pad']['x_mm'] / 2.0
+        top_y = -BODY_OFFSET_Y_MM - SILK_LINE_NUDGE
+        bottom_y = PAD_1_Y_MM - variant['pad']['y_mm'] / 2.0 - other_magic_number * SILK_LINE_NUDGE
+        top_marker = [[right_x, top_y], [middle_x, top_y], [middle_x, bottom_y], [left_x, bottom_y]]
         m.append(PolygoneLine(polygone=top_marker, layer=layer, width=SILK_LINE_WIDTH_MM))
+        top_y = -top_y
+        bottom_y = -bottom_y
+        left_x = DEVICE_OFFSET_X_MM - TAB_X_MM - BODY_X_MM - magic_number
+        bottom_marker = [[right_x, top_y], [middle_x, top_y], [middle_x, bottom_y], [left_x, bottom_y]]
         m.append(PolygoneLine(polygone=bottom_marker, layer=layer, width=SILK_LINE_WIDTH_MM))
         return m
 
 
-
+    def draw_pads(m):
+        for pin in range(1, variant['pins'] + 1):
+            if not (pin == CENTRE_PIN and cut_pin):
+                kicad_mod.append(Pad(number=pin, type=Pad.TYPE_SMT, shape=Pad.SHAPE_RECT,\
+                                     at=[PAD_1_X_MM, PAD_1_Y_MM + (pin - 1) * variant['pitch_mm']],\
+                                     size=[variant['pad']['x_mm'], variant['pad']['y_mm']], \
+                                     layers=Pad.LAYERS_SMT))
+        tab_layers = Pad.LAYERS_SMT[:]
+        if SPLIT_PASTE:
+            tab_layers.remove('F.Paste')
+        paste_layers = Pad.LAYERS_SMT[:]
+        paste_layers.remove('F.Mask')
+        kicad_mod.append(Pad(number=TAB_PIN_NUMBER, type=Pad.TYPE_SMT, shape=Pad.SHAPE_RECT,\
+                             at=[TAB_POS_X_MM, TAB_POS_Y_MM],\
+                             size=[base['footprint']['tab']['x_mm'], base['footprint']['tab']['y_mm']], \
+                             layers=tab_layers))
+        if SPLIT_PASTE:
+            gutter_mm = base['footprint']['paste_gutter_mm']
+            paste_x_mm = (base['footprint']['tab']['x_mm'] - gutter_mm) / 2.0
+            paste_y_mm = (base['footprint']['tab']['y_mm'] - gutter_mm) / 2.0
+            paste_offset_x_mm = (paste_x_mm + gutter_mm) / 2.0
+            paste_offset_y_mm = (paste_y_mm + gutter_mm) / 2.0
+            left_x = TAB_POS_X_MM - paste_offset_x_mm
+            right_x = TAB_POS_X_MM + paste_offset_x_mm
+            top_y = TAB_POS_Y_MM - paste_offset_y_mm
+            bottom_y = TAB_POS_Y_MM + paste_offset_y_mm
+            for pad_xy in [[right_x, bottom_y], [left_x, top_y], [right_x, top_y], [left_x, bottom_y]]:
+                kicad_mod.append(Pad(number=TAB_PIN_NUMBER, type=Pad.TYPE_SMT, shape=Pad.SHAPE_RECT,\
+                                     at=pad_xy, size=[paste_x_mm, paste_y_mm], layers=paste_layers))
+        return m
 
 
     # initialise footprint
     kicad_mod = Footprint(NAME)
-    kicad_mod.setDescription('{bd:s}, {vd:s}'.format(bd=base['description'], vd=variant['datasheet']))
-    kicad_mod.setTags('{bk:s} {vk:s}'.format(bk=base['keywords'], vk=variant['keywords']))
-    kicad_mod.setAttribute('smd')
-
-    # set general values
-    kicad_mod.append(Text(type='reference', text='REF**', size=[1,1], at=[LABEL_X_MM, -LABEL_Y_MM], layer='F.SilkS'))
-    kicad_mod.append(Text(type='user', text='%R', size=[1,1], at=[0, 0], layer='F.Fab'))
-    kicad_mod.append(Text(type='value', text=NAME, at=[LABEL_X_MM, LABEL_Y_MM], layer='F.Fab'))
+    kicad_mod = add_properties(kicad_mod)
+    kicad_mod = add_labels(kicad_mod)
 
     # create pads
-    for pin in range(1, variant['pins'] + 1):
-        if not (pin == CENTRE_PIN and cut_pin):
-            kicad_mod.append(Pad(number=pin, type=Pad.TYPE_SMT, shape=Pad.SHAPE_RECT,\
-                                 at=[PAD_1_X_MM, PAD_1_Y_MM + (pin - 1) * variant['pitch_mm']],\
-                                 size=[variant['pad']['x_mm'], variant['pad']['y_mm']], \
-                                 layers=Pad.LAYERS_SMT))
-    tab_layers = Pad.LAYERS_SMT[:]
-    if SPLIT_PASTE:
-        tab_layers.remove('F.Paste')
-    paste_layers = Pad.LAYERS_SMT[:]
-    paste_layers.remove('F.Mask')
-    kicad_mod.append(Pad(number=TAB_PIN_NUMBER, type=Pad.TYPE_SMT, shape=Pad.SHAPE_RECT,\
-                         at=[TAB_POS_X_MM, TAB_POS_Y_MM],\
-                         size=[base['footprint']['tab']['x_mm'], base['footprint']['tab']['y_mm']], \
-                         layers=tab_layers))
-    if SPLIT_PASTE:
-        paste_x_mm = (base['footprint']['tab']['x_mm'] - base['footprint']['paste_gutter_mm']) / 2.0
-        paste_y_mm = (base['footprint']['tab']['y_mm'] - base['footprint']['paste_gutter_mm']) / 2.0
-        paste_offset_x_mm = (paste_x_mm + base['footprint']['paste_gutter_mm']) / 2.0
-        paste_offset_y_mm = (paste_y_mm + base['footprint']['paste_gutter_mm']) / 2.0
-        kicad_mod.append(Pad(number=TAB_PIN_NUMBER, type=Pad.TYPE_SMT, shape=Pad.SHAPE_RECT,\
-                             at=[TAB_POS_X_MM + paste_offset_x_mm, TAB_POS_Y_MM + paste_offset_y_mm],\
-                             size=[paste_x_mm, paste_y_mm], \
-                             layers=paste_layers))
-        kicad_mod.append(Pad(number=TAB_PIN_NUMBER, type=Pad.TYPE_SMT, shape=Pad.SHAPE_RECT,\
-                             at=[TAB_POS_X_MM - paste_offset_x_mm, TAB_POS_Y_MM - paste_offset_y_mm],\
-                             size=[paste_x_mm, paste_y_mm], \
-                             layers=paste_layers))
-        kicad_mod.append(Pad(number=TAB_PIN_NUMBER, type=Pad.TYPE_SMT, shape=Pad.SHAPE_RECT,\
-                             at=[TAB_POS_X_MM + paste_offset_x_mm, TAB_POS_Y_MM - paste_offset_y_mm],\
-                             size=[paste_x_mm, paste_y_mm], \
-                             layers=paste_layers))
-        kicad_mod.append(Pad(number=TAB_PIN_NUMBER, type=Pad.TYPE_SMT, shape=Pad.SHAPE_RECT,\
-                             at=[TAB_POS_X_MM - paste_offset_x_mm, TAB_POS_Y_MM + paste_offset_y_mm],\
-                             size=[paste_x_mm, paste_y_mm], \
-                             layers=paste_layers))
+    kicad_mod = draw_pads(kicad_mod)
 
     # create fab outline 
     kicad_mod = draw_outline(kicad_mod, 'F.Fab')
 
     # create silkscreen marks and pin 1 marker 
     kicad_mod = draw_markers(kicad_mod, 'F.SilkS')
-
 
     # create courtyard
     kicad_mod.append(RectLine(start=[-COURTYARD_OFFSET_X_MM, -COURTYARD_OFFSET_Y_MM],\
