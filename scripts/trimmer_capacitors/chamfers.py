9e4c6e8c2178dc5e737165b228f2ec6b5f437ba9
hackscribble <hackscribble@outlook.com>
2017-05-06 11:06:39 +0100

    Updated function.

--- scripts/trimmer_capacitors/chamfers.py
+++ scripts/trimmer_capacitors/chamfers.py
@@ -1,20 +1,4 @@
-# KicadModTree is free software: you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# KicadModTree is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with kicad-footprint-generator. If not, see < http://www.gnu.org/licenses/ >.
-#
-# (C) 2016 by Thomas Pointhuber, <thomas.pointhuber@gmx.at>
-
 from KicadModTree import *
-
 
 def add_rect_chamfer(m, start_pos, end_pos, layer, width, offset=(0,0), chamfers=[]):
 
