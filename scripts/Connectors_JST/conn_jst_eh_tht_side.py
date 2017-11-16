35cad5eee6c6ed2f429bfad8fd49703ce119fc85
Rene Poeschl <r.poeschl@student.tugraz.at>
2017-11-16 17:10:47 +0100

    Number of rows is a variable. No magic number required.

--- scripts/Connectors_JST/conn_jst_eh_tht_side.py
+++ scripts/Connectors_JST/conn_jst_eh_tht_side.py
@@ -30,7 +30,7 @@ def generate_one_footprint(pincount, configuration):
     orientation_str = configuration['orientation_options'][orientation]
     footprint_name = configuration['fp_name_format_string'].format(man=manufacturer,
         series=series,
-        mpn=mpn, num_rows=1, pins_per_row=pincount,
+        mpn=mpn, num_rows=number_of_rows, pins_per_row=pincount,
         pitch=pitch, orientation=orientation_str)
 
     kicad_mod = Footprint(footprint_name)
