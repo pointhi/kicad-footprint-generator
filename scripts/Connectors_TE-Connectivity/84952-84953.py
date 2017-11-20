9b7e0d3029d5b69bb2746608594de9518215dfa0
evanshultz <evanshultz@users.noreply.github.com>
2017-11-20 10:49:45 -0800

    Removed one useless line of code

--- scripts/Connectors_TE-Connectivity/84952-84953.py
+++ scripts/Connectors_TE-Connectivity/84952-84953.py
@@ -48,7 +48,6 @@ if __name__ == '__main__':
     parser.add_argument('pincount', help='number of pins of the connector', type=int, action='append', nargs='?')
     parser.add_argument('partnumber', help='suffix of 502426 series number (e.g. 0810)', type=str, action='append', nargs='?')
     parser.add_argument('-v', '--verbose', help='show extra information while generating the footprint', action='store_true')
-    parser.parse_args()
     args = parser.parse_args()
     
     # determine if the user entered valid arguments
