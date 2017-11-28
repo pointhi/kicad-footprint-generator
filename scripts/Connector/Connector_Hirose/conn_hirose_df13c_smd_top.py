5e49f566b316c95a89dc0f31280f80fadceb7a6a
Rene Poeschl <r.poeschl@student.tugraz.at>
2017-11-28 20:47:19 +0100

    Fix some minor errors

--- scripts/Connector/Connector_Hirose/conn_hirose_df13c_smd_top.py
+++ scripts/Connector/Connector_Hirose/conn_hirose_df13c_smd_top.py
@@ -62,6 +62,7 @@ def generate_one_footprint(idx, pins, configuration):
     footprint_name = footprint_name.replace("__",'_')
 
     kicad_mod = Footprint(footprint_name)
+    kicad_mod.setAttribute('smd')
     kicad_mod.setDescription("Molex {:s}, {:s}, {:d} Pins per row ({:s}), generated with kicad-footprint-generator".format(series_long, mpn, pins_per_row, datasheet))
     kicad_mod.setTags(configuration['keyword_fp_string'].format(series=series,
         orientation=orientation_str, man=manufacturer,
