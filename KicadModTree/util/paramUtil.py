7e9fc929f669d98a9abb62762fed94d52603e696
Rene Pöschl <r.poeschl@student.tugraz.at>
2018-05-19 23:45:33 +0200

    Param helper utils more powerfull
    - controlled to vector now allows the limit per direction
    - to achieve this, a toFloatArray function was added
    - The toFloatArray and toIntArray functions share everything except
      type conversion. For this reason a toNumberArray function exists
      that allows selection of the type.

--- KicadModTree/util/paramUtil.py
+++ KicadModTree/util/paramUtil.py
@@ -18,15 +18,16 @@
 from KicadModTree.Vector import *
 
 
-def toIntArray(value, length=2, min_value=1):
-    r""" Convert value into an array of ints of given length
+def toNumberArray(value, length=2, min_value=1, member_type=int):
+    r""" Convert value into an array of given type with given length
 
     :param value:
         Possible input types:
           * numnber (int or float) -> returns array filled with copies of value
           * dict -> arreay created with values for keys 'x', 'y', 'z'.
             dict only supported for len 2 and 3
-          * list or tuple -> tuncated to length
+          * list or tuple -> truncated to length
+          * Vector -> truncated to lenght
 
     :param length: (``int``) -- default: 2
         Defines the length of the resulting array
@@ -34,6 +35,9 @@ def toIntArray(value, length=2, min_value=1):
     :min_value: (``int``) -- default: 1
         Defines the minimum alowed value (raise value error if too low)
         None -> No check
+
+    :param mamber_type: (``type``) -- default: <type: int>
+        all members of the array will be converted to this type
     """
     if type(value) in [int, float]:
         result = [value for i in range(length)]
@@ -46,10 +50,14 @@ def toIntArray(value, length=2, min_value=1):
     elif type(value) in [list, tuple]:
         if len(value) >= length:
             result = value[:length]
+    elif type(value) in [Vector2D, Vector3D]:
+        if len(value) < length:
+            raise TypeError('Vector dimensions ({}) are too low. Must be at least {}'.format(len(value), length))
+        result = list(value)
     else:
         raise TypeError('Unsupported type: {}'.format(type(value)))
 
-    result = [int(v) for v in result]
+    result = [member_type(v) for v in result]
 
     if min_value is not None:
         for v in result:
@@ -57,6 +65,48 @@ def toIntArray(value, length=2, min_value=1):
                 raise ValueError("Value ({}) too small. Linit is {}.".format(v, min_value))
 
     return result
+
+
+def toIntArray(value, length=2, min_value=1):
+    r""" Convert value into an array of ints of given length
+
+    :param value:
+        Possible input types:
+          * numnber (int or float) -> returns array filled with copies of value
+          * dict -> arreay created with values for keys 'x', 'y', 'z'.
+            dict only supported for len 2 and 3
+          * list or tuple -> truncated to length
+          * Vector -> truncated to lenght
+
+    :param length: (``int``) -- default: 2
+        Defines the length of the resulting array
+
+    :min_value: (``int``) -- default: 1
+        Defines the minimum alowed value (raise value error if too low)
+        None -> No check
+    """
+    return toNumberArray(value, length, min_value, member_type=int)
+
+
+def toFloatArray(value, length=2, min_value=1):
+    r""" Convert value into an array of floats of given length
+
+    :param value:
+        Possible input types:
+          * numnber (int or float) -> returns array filled with copies of value
+          * dict -> arreay created with values for keys 'x', 'y', 'z'.
+            dict only supported for len 2 and 3
+          * list or tuple -> truncated to length
+          * Vector -> truncated to lenght
+
+    :param length: (``int``) -- default: 2
+        Defines the length of the resulting array
+
+    :min_value: (``int``) -- default: 1
+        Defines the minimum alowed value (raise value error if too low)
+        None -> No check
+    """
+    return toNumberArray(value, length, min_value, member_type=int)
 
 
 def toVectorUseCopyIfNumber(value, length=2, low_limit=None, must_be_larger=True):
@@ -85,9 +135,12 @@ def toVectorUseCopyIfNumber(value, length=2, low_limit=None, must_be_larger=True
         result = value
 
     if low_limit is not None:
+        limits = toFloatArray(low_limit, length, min_value=None)
+        i = 0
         for v in result if type(result) is not dict else result.values():
-            if v < low_limit or (must_be_larger and v <= low_limit):
+            if v < limits[i] or (must_be_larger and v <= limits[i]):
                 raise ValueError("Value ({}) too small. Linit is {}.".format(v, low_limit))
+            i += 1
 
     if length == 2:
         return Vector2D(result)
