a7653e15009d6cca241bba5308262cf68f51fb3c
Antonio Vázquez <antoniovazquezblanco@gmail.com>
2018-05-24 11:26:15 +0200

    Revert "Connector_Harwin: Generate pin 1 fill as a pad in SilkS"
    This reverts commit 693317a9080b4695dd4d3fb4f7423f4fe5ffa375.

--- scripts/Connector/Connector_Harwin/m20-89xx.py
+++ scripts/Connector/Connector_Harwin/m20-89xx.py
@@ -140,8 +140,8 @@ def gen_footprint(pinnum, manpart, configuration):
 	cy_offset = configuration['courtyard_offset']['connector']
 	cy_grid = configuration['courtyard_grid']
 	bounding_box={
-		'left': -2.5/2,
-		'right': 12.3,
+		'left': -2.5/2-configuration['silk_pad_clearance']-configuration['silk_line_width']/2,
+		'right': 12.3+configuration['silk_line_width']/2,
 		'top': -pitch-2.54/2-configuration['silk_line_width']+2.54,
 		'bottom': -pitch-2.54/2+2.54*pinnum+configuration['silk_line_width']+2.54,
 	}
@@ -163,8 +163,8 @@ def gen_footprint(pinnum, manpart, configuration):
 	body_edge={
         'left': 3.8,
         'right': 6.3,
-        'top': -2.54/2,
-        'bottom': -2.54/2+2.54*pinnum,
+        'top': -pitch-2.54/2-silkslw+2.54,
+        'bottom': -pitch-2.54/2+2.54*pinnum+silkslw+2.54,
     }
 	addTextFields(kicad_mod=kicad_mod, configuration=configuration, body_edges=body_edge,
         courtyard={'top':cy_top, 'bottom':cy_bottom}, fp_name=footprint_name, text_y_inside_position='center', allow_rotation=True)
