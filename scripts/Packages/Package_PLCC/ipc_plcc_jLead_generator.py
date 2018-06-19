dce6b5ce7d8ba0cc83052856ba2c6dac4b8bd856
Rene Pöschl <r.poeschl@student.tugraz.at>
2018-06-20 00:43:31 +0200

    PLCC has pin 1 in the middle of the top row not at the corner

--- scripts/Packages/Package_PLCC/ipc_plcc_jLead_generator.py
+++ scripts/Packages/Package_PLCC/ipc_plcc_jLead_generator.py
@@ -50,41 +50,34 @@ class QFP():
         F = self.configuration.get('manufacturing_tolerance', 0.1)
         P = self.configuration.get('placement_tolerance', 0.05)
 
-        if 'overall_size_x_max' in device_params:
-            overall_x_tol = device_params['overall_size_x_max'] - device_params['overall_size_x_min']
-            overall_x_min = device_params['overall_size_x_min']
-        else:
-            overall_x_tol = 0
-            overall_x_min = device_params['overall_size_x']
-
-        if 'overall_size_y_max' in device_params:
-            overall_y_tol = device_params['overall_size_y_max'] - device_params['overall_size_y_min']
-            overall_y_min = device_params['overall_size_y_min']
-        else:
-            overall_y_tol = 0
-            overall_y_min = device_params['overall_size_y']
-
-
-        if 'lead_len_min' in device_params:
-            lead_len_tol = device_params['lead_len_max'] - device_params['lead_len_min']
-
         lead_width_tol = device_params['lead_width_max'] - device_params['lead_width_min']
 
-        def calcPadLength(overall_min, overall_tol, lead_inside=None):
+        def calcPadLength(overall, body, lead_inside=None, lead_center=None):
+            overall_tol = overall['max']-overall['min']
             if lead_inside:
-                Tmin = (device_params['overall_size_x_max'] - lead_inside['max'])/2
-                Tmax = (device_params['overall_size_x_min'] - lead_inside['min'])/2
+                Tmin = (overall['max'] - lead_inside['max'])/2
+                Tmax = (overall['min'] - lead_inside['min'])/2
                 Ttol = Tmax - Tmin
-                Stol_RMS = math.sqrt(overall_tol**2+2*(Ttol**2))
+                inside_tol = lead_inside['max'] - lead_inside['min']
+                Stol_RMS = math.sqrt(overall_tol**2+inside_tol**2)
                 Smax_RMS = lead_inside['min'] + Stol_RMS
+            elif lead_center:
+                body_tol = body['max']-body['min']
+                Tmin = (body['max'] - lead_center['max'])
+                Tmax = (body['min'] - lead_center['min'])
+                Ttol = Tmax - Tmin
+                center_tol = lead_center['max'] - lead_center['min']
+                Stol_RMS = math.sqrt(body_tol**2+center_tol**2)
+                Smax_RMS = body['max'] - 2*Tmin + Stol_RMS
             else:
+                lead_len_tol = device_params['lead_len_max'] - device_params['lead_len_min']
                 Stol_RMS = math.sqrt(overall_tol**2+2*(lead_len_tol**2))
-                Smin = overall_min - 2*device_params['lead_len_max']
+                Smin = overall['min'] - 2*device_params['lead_len_max']
                 Smax_RMS = Smin + Stol_RMS
 
             Gmin = Smax_RMS - 2*ipc_data['heel'] - math.sqrt(Stol_RMS**2 + F**2 + P**2)
 
-            Zmax = overall_min + 2*ipc_data['toe'] + math.sqrt(overall_tol**2 + F**2 + P**2)
+            Zmax = overall['min'] + 2*ipc_data['toe'] + math.sqrt(overall_tol**2 + F**2 + P**2)
 
             Zmax = roundToBase(Zmax, ipc_round_base['toe'])
             Gmin = roundToBase(Gmin, ipc_round_base['heel'])
@@ -92,28 +85,75 @@ class QFP():
             Zmax += device_params.get('pad_length_addition', 0)
 
             return Gmin, Zmax
+        overall_x={
+            'min':device_params.get('overall_size_x_min', device_params.get('overall_size_x')),
+            'max':device_params.get('overall_size_x_max', device_params.get('overall_size_x'))
+            }
+        overall_y={
+            'min':device_params.get('overall_size_y_min', device_params.get('overall_size_y')),
+            'max':device_params.get('overall_size_y_max', device_params.get('overall_size_y'))
+            }
 
-        if 'lead_len_min' in device_params:
-            Gmin_x, Zmax_x = calcPadLength(overall_x_min, overall_x_tol)
-            Gmin_y, Zmax_y = calcPadLength(overall_y_min, overall_y_tol)
+        body_x={
+            'min':device_params.get('body_size_x_min', device_params.get('body_size_x')),
+            'max':device_params.get('body_size_x_max', device_params.get('body_size_x'))
+            }
+        body_y={
+            'min':device_params.get('body_size_y_min', device_params.get('body_size_y')),
+            'max':device_params.get('body_size_y_max', device_params.get('body_size_y'))
+            }
+
+        if 'lead_inside_x_min' in device_params:
+            Gmin_x, Zmax_x = calcPadLength(
+                overall=overall_x,
+                body=body_x,
+                lead_inside={
+                    'min':device_params['lead_inside_x_min'],
+                    'max':device_params['lead_inside_x_max']}
+                 )
+            Gmin_y, Zmax_y = calcPadLength(
+                overall=overall_y,
+                body=body_y,
+                lead_inside={
+                    'min':device_params['lead_inside_y_min'],
+                    'max':device_params['lead_inside_y_max']}
+                )
+        elif 'lead_center_distance_x_min' in device_params:
+            Gmin_x, Zmax_x = calcPadLength(
+                overall=overall_x,
+                body=body_x,
+                lead_center={
+                    'min':device_params['lead_center_distance_x_min'],
+                    'max':device_params['lead_center_distance_x_max']}
+                 )
+            Gmin_y, Zmax_y = calcPadLength(
+                overall=overall_y,
+                body=body_y,
+                lead_center={
+                    'min':device_params['lead_center_distance_y_min'],
+                    'max':device_params['lead_center_distance_y_max']}
+                 )
         else:
             Gmin_x, Zmax_x = calcPadLength(
-                overall_x_min, overall_x_tol,
-                {'min':device_params['lead_inside_x_min'],
-                 'max':device_params['lead_inside_x_max']})
+                overall=overall_x,
+                body=body_x
+                )
+
             Gmin_y, Zmax_y = calcPadLength(
-                overall_y_min, overall_y_tol,
-                {'min':device_params['lead_inside_y_min'],
-                 'max':device_params['lead_inside_y_max']})
+                overall=overall_y,
+                body=body_y
+                )
 
 
         Xmax = device_params['lead_width_min'] + 2*ipc_data['side'] + math.sqrt(lead_width_tol**2 + F**2 + P**2)
         Xmax = roundToBase(Xmax, ipc_round_base['side'])
 
         Pad = {}
+        Pad['first'] = {'start':[-((device_params['num_pins_x']-1)%2)/2*device_params['pitch'], -(Zmax_y+Gmin_y)/4], 'size':[Xmax,(Zmax_y-Gmin_y)/2]}
+
         Pad['left'] = {'center':[-(Zmax_x+Gmin_x)/4, 0], 'size':[(Zmax_x-Gmin_x)/2,Xmax]}
         Pad['right'] = {'center':[(Zmax_x+Gmin_x)/4, 0], 'size':[(Zmax_x-Gmin_x)/2,Xmax]}
-        Pad['top'] = {'center':[0,-(Zmax_y+Gmin_y)/4], 'size':[Xmax,(Zmax_y-Gmin_y)/2]}
+        Pad['top'] = {'start':[(device_params['num_pins_x']-1)/2*device_params['pitch'],-(Zmax_y+Gmin_y)/4], 'size':[Xmax,(Zmax_y-Gmin_y)/2]}
         Pad['bottom'] = {'center':[0,(Zmax_y+Gmin_y)/4], 'size':[Xmax,(Zmax_y-Gmin_y)/2]}
 
         return Pad
@@ -213,6 +253,15 @@ class QFP():
             initial= init,
             type=Pad.TYPE_SMT,
             layers=Pad.LAYERS_SMT,
+            pincount=int(math.ceil(device_params['num_pins_x']/2)),
+            x_spacing=-device_params['pitch'], y_spacing=0,
+            **pad_details['first'], **pad_shape_details))
+
+        init += int(math.ceil(device_params['num_pins_x']/2))
+        kicad_mod.append(PadArray(
+            initial= init,
+            type=Pad.TYPE_SMT,
+            layers=Pad.LAYERS_SMT,
             pincount=device_params['num_pins_y'],
             x_spacing=0, y_spacing=device_params['pitch'],
             **pad_details['left'], **pad_shape_details))
@@ -240,27 +289,28 @@ class QFP():
             initial= init,
             type=Pad.TYPE_SMT,
             layers=Pad.LAYERS_SMT,
-            pincount=device_params['num_pins_x'],
+            pincount=int(math.floor(device_params['num_pins_x']/2)),
             y_spacing=0, x_spacing=-device_params['pitch'],
             **pad_details['top'], **pad_shape_details))
 
 
         body_edge = {
-            'left': -device_params['body_size_x']/2,
-            'right': device_params['body_size_x']/2,
-            'top': -device_params['body_size_y']/2,
-            'bottom': device_params['body_size_y']/2
+            'left': -size_x/2,
+            'right': size_x/2,
+            'top': -size_y/2,
+            'bottom': size_y/2
             }
 
         bounding_box = {
             'left': pad_details['left']['center'][0] - pad_details['left']['size'][0]/2,
             'right': pad_details['right']['center'][0] + pad_details['right']['size'][0]/2,
-            'top': pad_details['top']['center'][1] - pad_details['top']['size'][1]/2,
+            'top': pad_details['top']['start'][1] - pad_details['top']['size'][1]/2,
             'bottom': pad_details['bottom']['center'][1] + pad_details['bottom']['size'][1]/2
         }
 
 
         pad_width = pad_details['top']['size'][0]
+        p1_x = pad_details['first']['start'][0]
 
         # ############################ SilkS ##################################
 
@@ -278,11 +328,6 @@ class QFP():
             {'x': body_edge['left']-silk_offset, 'y': body_edge['top']-silk_offset},
             {'x': body_edge['left']-silk_offset, 'y': sy1}
         ]
-
-        kicad_mod.append(PolygoneLine(
-            polygone=poly_silk,
-            width=configuration['silk_line_width'],
-            layer="F.SilkS"))
         kicad_mod.append(PolygoneLine(
             polygone=poly_silk,
             width=configuration['silk_line_width'],
@@ -296,23 +341,32 @@ class QFP():
             width=configuration['silk_line_width'],
             layer="F.SilkS", x_mirror=0, y_mirror=0))
 
-        kicad_mod.append(Line(
-            start={'x': body_edge['left']-silk_offset, 'y': sy1},
-            end={'x': bounding_box['left'], 'y': sy1},
+        silk_off_45 = silk_offset/sqrt(2)
+        poly_silk_tl = [
+            {'x': sx1, 'y': body_edge['top']-silk_offset},
+            {'x': body_edge['left']+device_params['body_chamfer']-silk_off_45, 'y': body_edge['top']-silk_offset},
+            {'x': body_edge['left']-silk_offset, 'y': body_edge['top']+device_params['body_chamfer']-silk_off_45},
+            {'x': body_edge['left']-silk_offset, 'y': sy1}
+        ]
+        kicad_mod.append(PolygoneLine(
+            polygone=poly_silk_tl,
             width=configuration['silk_line_width'],
             layer="F.SilkS"))
 
         # # ######################## Fabrication Layer ###########################
 
         fab_bevel_size = min(configuration['fab_bevel_size_absolute'], configuration['fab_bevel_size_relative']*max(size_x, size_y))
-
+        fab_bevel_y = fab_bevel_size/sqrt(2)
         poly_fab = [
-            {'x': body_edge['left']+fab_bevel_size, 'y': body_edge['top']},
+            {'x': p1_x, 'y': body_edge['top']+fab_bevel_y},
+            {'x': p1_x+fab_bevel_size/2, 'y': body_edge['top']},
             {'x': body_edge['right'], 'y': body_edge['top']},
             {'x': body_edge['right'], 'y': body_edge['bottom']},
             {'x': body_edge['left'], 'y': body_edge['bottom']},
-            {'x': body_edge['left'], 'y': body_edge['top']+fab_bevel_size},
-            {'x': body_edge['left']+fab_bevel_size, 'y': body_edge['top']},
+            {'x': body_edge['left'], 'y': body_edge['top']+device_params['body_chamfer']},
+            {'x': body_edge['left']+device_params['body_chamfer'], 'y': body_edge['top']},
+            {'x': p1_x-fab_bevel_size/2, 'y': body_edge['top']},
+            {'x': p1_x, 'y': body_edge['top']+fab_bevel_y}
         ]
 
         kicad_mod.append(PolygoneLine(
@@ -324,12 +378,14 @@ class QFP():
 
         off = ipc_data_set['courtyard']
         grid = configuration['courtyard_grid']
+        off_45 = off*math.tan(math.radians(45.0/2))
 
         cy1=roundToBase(bounding_box['top']-off, grid)
         cy2=roundToBase(body_edge['top']-off, grid)
         cy3=-roundToBase(
             device_params['pitch']*(device_params['num_pins_y']-1)/2.0
             + pad_width/2.0 + off, grid)
+        cy4=roundToBase(body_edge['top']+device_params['body_chamfer']-off_45, grid)
 
 
 
@@ -338,6 +394,7 @@ class QFP():
             + pad_width/2.0 + off, grid)
         cx2=roundToBase(body_edge['left']-off, grid)
         cx3=roundToBase(bounding_box['left']-off, grid)
+        cx4=roundToBase(body_edge['left']+device_params['body_chamfer']-off_45, grid)
 
 
         crty_poly_tl = [
@@ -349,8 +406,7 @@ class QFP():
             {'x':cx3, 'y':cy3},
             {'x':cx3, 'y':0}
         ]
-        kicad_mod.append(PolygoneLine(polygone=crty_poly_tl,
-            layer='F.CrtYd', width=configuration['courtyard_line_width']))
+
         kicad_mod.append(PolygoneLine(polygone=crty_poly_tl,
             layer='F.CrtYd', width=configuration['courtyard_line_width'],
             x_mirror=0))
@@ -360,6 +416,19 @@ class QFP():
         kicad_mod.append(PolygoneLine(polygone=crty_poly_tl,
             layer='F.CrtYd', width=configuration['courtyard_line_width'],
             x_mirror=0, y_mirror=0))
+
+        crty_poly_tl_ch = [
+            {'x':0, 'y':cy1},
+            {'x':cx1, 'y':cy1},
+            {'x':cx1, 'y':cy2},
+            {'x':cx4, 'y':cy2},
+            {'x':cx2, 'y':cy4},
+            {'x':cx2, 'y':cy3},
+            {'x':cx3, 'y':cy3},
+            {'x':cx3, 'y':0}
+        ]
+        kicad_mod.append(PolygoneLine(polygone=crty_poly_tl_ch,
+            layer='F.CrtYd', width=configuration['courtyard_line_width']))
 
         # ######################### Text Fields ###############################
 
