c85f2d847b2639df63fe1b994af2d52e0c99aee9
Rene Pöschl <r.poeschl@student.tugraz.at>
2018-06-19 20:54:07 +0200

    Rounding of values in documentation fields for terminal blocks

--- scripts/tools/footprint_scripts_terminal_blocks.py
+++ scripts/tools/footprint_scripts_terminal_blocks.py
@@ -65,8 +65,10 @@ def makeTerminalBlockStd(footprint_name, pins, rm, package_height, leftbottom_of
     text_t = text_size[0] * 0.15
 
 
-    description = "{2}, {0} pins, pitch {1:02}mm, size {3}x{4}mm^2, drill diamater {5}mm, pad diameter {6}mm, see {7}".format(pins, rm,classname_description, package_size[0], package_size[1], ddrill, max(pad), webpage)
-    tags = "THT {2} pitch {1:02}mm size {3}x{4}mm^2 drill {5}mm pad {6}mm".format(pins, rm,classname_description, package_size[0], package_size[1], ddrill, max(pad))
+    description = "{2}, {0:d} pins, pitch {1:.3g}mm, size {3:.3g}x{4:.3g}mm^2, drill diamater {5:.3g}mm, pad diameter {6:.3g}mm, see {7:s}"\
+        .format(pins, rm, classname_description, package_size[0], package_size[1], ddrill, max(pad), webpage)
+    tags = "THT {2} pitch {1:.3g}mm size {3:.3g}x{4:.3g}mm^2 drill {5:.3g}mm pad {6:.3g}mm"\
+        .format(pins, rm, classname_description, package_size[0], package_size[1], ddrill, max(pad))
 
     if len(script_generated_note)>0:
         description=description+", "+script_generated_note
@@ -283,8 +285,10 @@ def makeTerminalBlockVertical(footprint_name, pins, rm, package_height, leftbott
     text_t = text_size[0] * 0.15
 
 
-    description = "{2}, vertical (cable from top), {0} pins, pitch {1:02}mm, size {3}x{4}mm^2, drill diamater {5}mm, pad diameter {6}mm, see {7}, script-generated with ".format(pins, rm,classname_description, package_size[0], package_size[1], ddrill, max(pad), webpage)
-    tags = "THT {2} vertical pitch {1:02}mm size {3}x{4}mm^2 drill {5}mm pad {6}mm".format(pins, rm,classname_description, package_size[0], package_size[1], ddrill, max(pad))
+    description = "{2}, vertical (cable from top), {0:d} pins, pitch {1:.3g}mm, size {3:.3g}x{4:.3g}mm^2, drill diamater {5:.3g}mm, pad diameter {6:.3g}mm, see {7}, script-generated with "\
+        .format(pins, rm, classname_description, package_size[0], package_size[1], ddrill, max(pad), webpage)
+    tags = "THT {2} vertical pitch {1:.3g}mm size {3:.3g}x{4:.3g}mm^2 drill {5:.3g}mm pad {6:.3g}mm"\
+        .format(pins, rm, classname_description, package_size[0], package_size[1], ddrill, max(pad))
 
     if len(script_generated_note)>0:
         description=description+", "+script_generated_note
@@ -503,8 +507,10 @@ def makeTerminalBlock45Degree(footprint_name, pins, rm, package_height, leftbott
     text_t = text_size[0] * 0.15
 
 
-    description = "{2}, 45Degree (cable under 45degree), {0} pins, pitch {1:02}mm, size {3}x{4}mm^2, drill diamater {5}mm, pad diameter {6}mm, see {7}, script-generated with ".format(pins, rm,classname_description, package_size[0], package_size[1], ddrill, max(pad), webpage)
-    tags = "THT {2} 45Degree pitch {1:02}mm size {3}x{4}mm^2 drill {5}mm pad {6}mm".format(pins, rm,classname_description, package_size[0], package_size[1], ddrill, max(pad))
+    description = "{2}, 45Degree (cable under 45degree), {0:d} pins, pitch {1:.3g}mm, size {3:.3g}x{4:.3g}mm^2, drill diamater {5:.3g}mm, pad diameter {6:.3g}mm, see {7}, script-generated with "\
+        .format(pins, rm,classname_description, package_size[0], package_size[1], ddrill, max(pad), webpage)
+    tags = "THT {2} 45Degree pitch {1:.3g}mm size {3:.3g}x{4:.3g}mm^2 drill {5:.3g}mm pad {6:.3g}mm"\
+        .format(pins, rm,classname_description, package_size[0], package_size[1], ddrill, max(pad))
 
     if len(script_generated_note)>0:
         description=description+", "+script_generated_note
@@ -733,8 +739,10 @@ def makeScrewTerminalSingleStd(footprint_name, block_size, block_offset, pins, d
     txt_offset=text_size[1]
 
 
-    description = "{2}, block size {3}x{4}mm^2, drill diamater {5}mm, {1} pads, pad diameter {6}mm, see {7}".format(0, len(pins),classname_description, block_size[0], block_size[1], ddrill, max(pad), webpage)
-    tags = "THT {2} size {3}x{4}mm^2 drill {5}mm pad {6}mm".format(0, len(pins),classname_description, block_size[0], block_size[1], ddrill, max(pad))
+    description = "{2}, block size {3:.3g}x{4:.3g}mm^2, drill diamater {5:.3g}mm, {1:d} pads, pad diameter {6:.3g}mm, see {7}"\
+        .format(0, len(pins), classname_description, block_size[0], block_size[1], ddrill, max(pad), webpage)
+    tags = "THT {2} size {3:.3g}x{4:.3g}mm^2 drill {5:.3g}mm pad {6:.3g}mm"\
+        .format(0, len(pins), classname_description, block_size[0], block_size[1], ddrill, max(pad))
 
     if len(script_generated_note)>0:
         description=description+", "+script_generated_note
