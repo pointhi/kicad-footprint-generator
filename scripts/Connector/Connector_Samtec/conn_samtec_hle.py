6fd2d00f7509d8ba16c231b4013ad4b9fed57d60
Rene Pöschl <r.poeschl@student.tugraz.at>
2018-08-18 21:26:44 +0200

    Samtec HLE transfered to rounded rectangle pad 1

--- scripts/Connector/Connector_Samtec/conn_samtec_hle.py
+++ scripts/Connector/Connector_Samtec/conn_samtec_hle.py
@@ -279,12 +279,21 @@ def generate_one_footprint(pins_per_row, variant, configuration):
                     drill=npth_drill, layers=Pad.LAYERS_NPTH))
 
     # Pads
+    optional_pad_params = {}
+    if not is_smd:
+        if configuration['kicad4_compatible']:
+            optional_pad_params['tht_pad1_shape'] = Pad.SHAPE_RECT
+        else:
+            optional_pad_params['tht_pad1_shape'] = Pad.SHAPE_ROUNDRECT
+
     kicad_mod.append(PadArray(start=[pad1_x, pad_row1_y], initial=1,
         pincount=pins_per_row, increment=2,  x_spacing=pitch, size=pad_size,
-        type=pad_type, shape=pad_shape, layers=pad_layer, drill=pad_drill))
+        type=pad_type, shape=pad_shape, layers=pad_layer, drill=pad_drill,
+        **optional_pad_params))
     kicad_mod.append(PadArray(start=[pad1_x, pad_row2_y], initial=2,
         pincount=pins_per_row, increment=2, x_spacing=pitch, size=pad_size,
-        type=pad_type, shape=pad_shape, layers=pad_layer, drill=pad_drill))
+        type=pad_type, shape=pad_shape, layers=pad_layer, drill=pad_drill,
+        **optional_pad_params))
 
     # ######################## Fabrication Layer ###########################
 
@@ -414,6 +423,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='use confing .yaml files to create footprints.')
     parser.add_argument('--global_config', type=str, nargs='?', help='the config file defining how the footprint will look like. (KLC)', default='../../tools/global_config_files/config_KLCv3.0.yaml')
     parser.add_argument('--series_config', type=str, nargs='?', help='the config file defining series parameters.', default='../conn_config_KLCv3.yaml')
+    parser.add_argument('--kicad4_compatible', action='store_true', help='Create footprints kicad 4 compatible')
     args = parser.parse_args()
 
     with open(args.global_config, 'r') as config_stream:
@@ -427,6 +437,9 @@ if __name__ == "__main__":
             configuration.update(yaml.load(config_stream))
         except yaml.YAMLError as exc:
             print(exc)
+
+    configuration['kicad4_compatible'] = args.kicad4_compatible
+
     for variant in variant_params:
         for pins_per_row in variant_params[variant]['pins_per_row_range']:
             generate_one_footprint(pins_per_row, variant, configuration)
