5a1495219a0d7e53580badc4dcd9c46683254118
Rene Pöschl <r.poeschl@student.tugraz.at>
2018-08-24 01:25:07 +0200

    handle pad too far away in inline direction
    This handles the case where the original moving point should be used as
    it is nearer to the fixed point then the calculated new point. (This is
    the case if the pad is far away in inline direction but not in normal
    direction)

--- scripts/tools/drawing_tools.py
+++ scripts/tools/drawing_tools.py
@@ -829,4 +829,7 @@ def nearestSilkPointOnOrtoLine(pad_size, pad_position, pad_radius, fixed_point, 
     if sign*(ep_new[inline_dir_idx] - fixed_point[inline_dir_idx]) <  min_lenght:
         return None
 
+    if math.fabs(ep_new[inline_dir_idx] - fixed_point[inline_dir_idx]) > math.fabs(moving_point[inline_dir_idx] - fixed_point[inline_dir_idx]):
+        return moving_point
+    
     return ep_new
