7a4befa8339b649e53e77fbd7ad8fcfcf64e520b
evanshultz <evan.shultz@gmail.com>
2018-09-09 07:34:29 -0700

    Fix pycodestyle errors/warning (I hope!)

--- KicadModTree/nodes/Footprint.py
+++ KicadModTree/nodes/Footprint.py
@@ -31,8 +31,8 @@ ADVANTAGES:
 
 # define in which order the general "lisp" operators are arranged
 render_order = ['descr', 'tags', 'attr', 'solder_mask_margin',
-                    'solder_paste_margin', 'solder_paste_ratio', 'fp_text',
-                    'fp_circle', 'fp_line', 'pad', 'model']
+                'solder_paste_margin', 'solder_paste_ratio', 'fp_text',
+                'fp_circle', 'fp_line', 'pad', 'model']
 # TODO: sort Text by type
 
 
@@ -71,6 +71,6 @@ class Footprint(Node):
 
     def setPasteMarginRatio(self, value):
         # paste_margin_ratio is unitless between 0 and 1 while GUI uses percentage
-        assert abs(value) <= 1,"Solder paste margin must be between -1 and 1. {} is too large.".format(value)
-        
+        assert abs(value) <= 1, "Solder paste margin must be between -1 and 1. {} is too large.".format(value)
+
         self.pasteMarginRatio = value
